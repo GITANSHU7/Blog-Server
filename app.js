b9ec5a71e1cdf2b11cac48d73c194d3ac497eefc
Gitanshu Gautam <31912912+GITANSHU7@users.noreply.github.com>
2024-11-24 00:49:17 +0530

    add vercel json

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
     }
-    const fileUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+    // const fileUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+    const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
     res.json({ url: fileUrl });
 });
 
